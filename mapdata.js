9cc5f66b7d3c77a6ee4ac84c270c0d1bbc265cf9
shane-webflower <158411192+shane-webflower@users.noreply.github.com>
2024-03-28 18:58:51 +0800

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -6,22 +6,24 @@ var simplemaps_ukmap_mapdata={
     background_transparent: "yes",
     border_color: "#ffffff",
     popups: "detect",
-    state_description: "",
-    state_color: "#F8EEDA",
-    state_hover_color: "darkorange",
-    state_url: "https://jellyfish.solar",
+    
+    //State defaults
+    state_description: "Click to go to your state!",
+    state_color: "F3BB56",
+    state_hover_color: "#f98200",
+    state_url: "https://simplemaps.com",
     border_size: 1.5,
     all_states_inactive: "yes",
     all_states_zoomable: "no",
     
     //Location defaults
-    location_description: "In Dorset House, Surrey",
+    location_description: "Location description",
     location_color: "#FF0067",
     location_opacity: 0.8,
     location_hover_opacity: 1,
     location_url: "",
-    location_size: "15",
-    location_type: "Circle",
+    location_size: 25,
+    location_type: "square",
     location_image_source: "frog.png",
     location_border_color: "#FFFFFF",
     location_border: 2,
@@ -30,9 +32,9 @@ var simplemaps_ukmap_mapdata={
     all_locations_hidden: "no",
     
     //Label defaults
-    label_color: "#000000",
+    label_color: "#000",
     label_hover_color: "#ffffff",
-    label_size: "8",
+    label_size: "7",
     label_font: "Avenir",
     hide_labels: "no",
     manual_zoom: "no",
@@ -63,567 +65,561 @@ var simplemaps_ukmap_mapdata={
     link_text: "View Website",
     state_image_url: "",
     state_image_position: "",
-    location_image_url: ""
+    location_image_url: "",
+    border_hover_color: "#C66C00"
   },
   state_specific: {
     GBNIR1: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Antrim",
       description: " "
     },
     GBNIR2: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Armagh",
       description: " "
     },
     GBNIR3: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Down",
       description: " "
     },
     GBNIR4: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Fermanagh",
       description: " "
     },
     GBNIR5: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Londonderry",
       description: " "
     },
     GBNIR6: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Tyrone ",
       description: " "
     },
     GBSCT1: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Aberdeen City",
       description: " "
     },
     GBSCT10: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "East Lothian",
       description: " "
     },
     GBSCT11: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "East Renfrewshire",
       description: " "
     },
     GBSCT12: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "City Of Edinburgh",
       description: " "
     },
     GBSCT13: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Na H-Eileanan An Iar",
       description: " "
     },
     GBSCT14: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Falkirk",
       description: " "
     },
     GBSCT15: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Fife",
       description: " "
     },
     GBSCT16: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Glasgow City",
       description: " "
     },
     GBSCT17: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Highland",
       description: " "
     },
     GBSCT18: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Inverclyde",
       description: " "
     },
     GBSCT19: {
-      color: "#ca8500",
+      color: "#ffedcd",
       name: "Midlothian",
       description: " "
     },
     GBSCT2: {
-      color: "#ca8500",
+      color: "#ffdb9a",
       name: "Aberdeenshire",
       description: " "
     },
     GBSCT20: {
-      color: "#ca8500",
+      color: "#ffdb9a",
       name: "Moray",
       description: " "
     },
     GBSCT21: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "North Ayrshire",
       description: " "
     },
     GBSCT22: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "North Lanarkshire",
       description: " "
     },
     GBSCT23: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Orkney Islands",
       description: " "
     },
     GBSCT24: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Perth And Kinross",
       description: " "
     },
     GBSCT25: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Renfrewshire",
       description: " "
     },
     GBSCT26: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Scottish Borders",
       description: " "
     },
     GBSCT27: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Shetland Islands",
       description: " "
     },
     GBSCT28: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "South Ayrshire",
       description: " "
     },
     GBSCT29: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "South Lanarkshire",
       description: " "
     },
     GBSCT3: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Angus",
       description: " "
     },
     GBSCT30: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Stirling",
       description: " "
     },
     GBSCT31: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "West Dunbartonshire",
       description: " "
     },
     GBSCT32: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "West Lothian",
       description: " "
     },
     GBSCT4: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Argyll And Bute",
       description: " "
     },
     GBSCT5: {
-      color: "#fda700",
+      color: "#ffdb9a",
       name: "Clackmannanshire",
       description: " "
     },
     GBSCT6: {
-      color: "#fda700",
+      color: "#ffc967",
       name: "Dumfries And Galloway",
       description: " "
     },
     GBSCT7: {
-      color: "#fda700",
+      color: "#ffc967",
       name: "Dundee City",
       description: " "
     },
     GBSCT8: {
-      color: "#fda700",
+      color: "#ffc967",
       name: "East Ayrshire",
       description: " "
     },
     GBSCT9: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "East Dunbartonshire",
       description: " "
     },
     UK01: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Bristol",
       description: " "
     },
     UK02: {
-      color: "#ffb931",
-      inactive: "yes",
+      color: "#ffc967",
       name: "Bedfordshire",
       description: " "
     },
     UK03: {
-      color: "#ffb931",
-      inactive: "yes",
+      color: "#ffc967",
+      inactive: "no",
       name: "Berkshire",
       description: " "
     },
     UK04: {
-      color: "#ffb931",
+      color: "#ffc967",
       inactive: "no",
       name: "Buckinghamshire",
       description: " "
     },
     UK05: {
-      color: "#ffb931",
-      inactive: "yes",
+      color: "#ffc967",
       name: "Cambridgeshire",
       description: " "
     },
     UK06: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Cheshire",
       description: " "
     },
     UK07: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Durham",
       description: " "
     },
     UK08: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Cornwall",
       description: " "
     },
     UK09: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Cumbria",
       description: " "
     },
     UK10: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Derbyshire",
       description: " "
     },
     UK11: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Devon",
       description: " "
     },
     UK12: {
-      color: "#ffb931",
+      color: "#ffc967",
       name: "Dorset",
       description: " "
     },
     UK13: {
-      color: "#ffb931",
+      color: "#ffb734",
       name: "Durham",
       description: " "
     },
     UK14: {
-      color: "#ffb931",
+      color: "#ffb734",
       inactive: "no",
       name: "East Sussex",
       description: " "
     },
     UK15: {
-      color: "#ffb931",
+      color: "#ffb734",
       inactive: "no",
       name: "Essex",
       description: " "
     },
     UK16: {
-      color: "#ffb931",
+      color: "#ffb734",
       name: "Gloucestershire",
       description: " "
     },
     UK17: {
-      color: "#ffb931",
+      color: "#ffb734",
       inactive: "no",
       name: "Greater London",
       description: " "
     },
     UK18: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Greater Manchester",
       description: " "
     },
     UK19: {
-      color: "#ffca64",
+      color: "#ffb734",
       inactive: "no",
       name: "Hampshire",
       description: " "
     },
     UK20: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Herefordshire",
       description: " "
     },
     UK21: {
-      color: "#ffca64",
+      color: "#ffb734",
       inactive: "no",
       name: "Hertfordshire",
       description: " "
     },
     UK22: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Kingston Upon Hull",
       description: " "
     },
     UK23: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Isle Of Wight",
       description: " "
     },
     UK24: {
-      color: "#ffca64",
+      color: "#ffb734",
       inactive: "no",
       name: "Kent",
       description: " "
     },
     UK25: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Lancashire",
       description: " "
     },
     UK26: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Leicestershire",
       description: " "
     },
     UK28: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Merseyside",
       description: " "
     },
     UK29: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "Norfolk",
       description: " "
     },
     UK30: {
-      color: "#ffca64",
+      color: "#ffb734",
       name: "North Yorkshire",
       description: " "
     },
     UK31: {
-      color: "#ffca64",
-      inactive: "yes",
-      name: "Northants",
+      color: "#ffa501",
+      name: "Buckinghamshire",
       description: " "
     },
     UK32: {
-      color: "#ffca64",
+      color: "#ffa501",
       name: "Lincolnshire",
       description: " "
     },
     UK33: {
-      color: "#ffca64",
+      color: "#ffa501",
       name: "Nottinghamshire",
       description: " "
     },
     UK34: {
-      color: "#ffca64",
-      inactive: "yes",
+      color: "#ffa501",
       name: "Oxfordshire",
       description: " "
     },
     UK35: {
-      color: "#ffca64",
+      color: "#ffa501",
       name: "Shropshire",
       description: " "
     },
     UK36: {
-      color: "#ffca64",
+      color: "#ffa501",
       name: "Somerset",
       description: " "
     },
     UK37: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "South Yorkshire",
       description: " "
     },
     UK38: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "Staffordshire",
       description: " "
     },
     UK39: {
-      color: "#ffdc97",
-      inactive: "yes",
+      color: "#ffa501",
       name: "Suffolk",
       description: " "
     },
     UK40: {
-      color: "#ffdc97",
+      color: "#ffa501",
       inactive: "no",
       name: "Surrey",
       description: " "
     },
     UK41: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "Tyne And Wear",
       description: " "
     },
     UK42: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "Warwickshire",
       description: " "
     },
     UK43: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "West Midlands",
       description: " "
     },
     UK44: {
-      color: "#ffdc97",
+      color: "#ffa501",
       inactive: "no",
-      name: "West Sussex",
+      name: "East Sussex",
       description: " "
     },
     UK45: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "West Yorkshire",
       description: " "
     },
     UK46: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "Wiltshire",
       description: " "
     },
     UK47: {
-      color: "#ffdc97",
+      color: "#ffa501",
       name: "Northumberland",
       description: " "
     },
     UK90: {
-      color: "#ffdc97",
       name: "Clwyd",
       description: " "
     },
     UK91: {
-      color: "#ffdc97",
       name: "Dyfed",
       description: " "
     },
     UK92: {
-      color: "#ffdc97",
       name: "Gwent",
       description: " "
     },
     UK93: {
-      color: "#ffdc97",
       name: "Gwynedd",
       description: " "
     },
     UK94: {
-      color: "#ffdc97",
       name: "Mid Glamorgan",
       description: " "
     },
     UK95: {
-      color: "#ffdc97",
       name: "Powys",
       description: " "
     },
     UK96: {
-      color: "#ffdc97",
       name: "South Glamorgan",
       description: " "
     },
     UK97: {
-      color: "#ffdc97",
       name: "West Glamorgan",
       description: " "
     },
     standard: {
-      color: "#ffb931",
       inactive: "yes",
       description: " "
     }
   },
   locations: {
     "0": {
-      lat: "51.310292",
-      lng: "-0.334675",
+      lat: "51.310321",
+      lng: "-0.334668",
       name: "Jellyfish HQ"
     }
   },
   labels: {
+    "0": {
+      name: "East Sussex",
+      x: 856.05556,
+      y: 1232.26231,
+      parent_id: "UK44"
+    },
     "1": {
-      name: "Surrey",
-      x: 773.1697,
-      y: 1191.63656,
-      parent_id: "UK40"
-    },
-    "2": {
-      name: "Essex",
-      x: 869.36364,
-      y: 1134.52141,
-      parent_id: "UK15"
-    },
-    "3": {
       name: "Kent",
-      x: 903.93333,
-      y: 1193.13959,
+      x: 866.38889,
+      y: 1193.79935,
       parent_id: "UK24"
     },
-    "4": {
-      x: 773.1697,
-      y: 1229.96384,
-      parent_id: "UK44",
-      name: "West Sussex"
+    "2": {
+      name: "East Sussex",
+      x: 785.44444,
+      y: 1228.81786,
+      parent_id: "UK44"
     },
-    "5": {
-      name: "Hertfordshire",
-      x: 788.95152,
-      y: 1115.73353,
-      parent_id: "UK21"
-    },
-    "10": {
+    "3": {
       name: "Hampshire",
-      x: 702.52727,
-      y: 1203.6608,
+      x: 699.33333,
+      y: 1218.48453,
       parent_id: "UK19"
     },
-    "11": {
-      name: "Buckinghamshire",
-      x: 758.13939,
-      y: 1142.03656,
-      parent_id: "UK04"
+    "4": {
+      name: "Surrey",
+      x: 765.92593,
+      y: 1190.92897,
+      parent_id: "UK40"
     },
-    "13": {
+    "5": {
       name: "Greater London",
-      x: 825.77576,
-      y: 1155.56384,
+      x: 813.57407,
+      y: 1165.66972,
       parent_id: "UK17"
     },
-    "14": {
-      name: "East Sussex",
-      x: 851.32727,
-      y: 1238.98202,
-      parent_id: "UK14"
+    "6": {
+      name: "Essex",
+      x: 858.35185,
+      y: 1118.02157,
+      parent_id: "UK15"
+    },
+    "7": {
+      name: "Hertfordshire",
+      x: 807.25926,
+      y: 1118.59564,
+      parent_id: "UK21"
+    },
+    "8": {
+      name: "Buckinghamshire",
+      x: 742.96296,
+      y: 1121.46601,
+      parent_id: "UK04"
+    },
+    "9": {
+      name: "Berkshire",
+      x: 728.03704,
+      y: 1170.83638,
+      parent_id: "UK03"
     }
   },
   regions: {
     "0": {
       states: [
+        "UK03",
         "UK04",
         "UK14",
         "UK15",
         "UK17",
         "UK19",
         "UK21",
-        "UK24",
         "UK40",
-        "UK44"
+        "UK44",
+        "UK24"
       ],
-      name: "South of England"
+      name: "South east"
     }
   },
   legend: {
@@ -631,99 +627,99 @@ var simplemaps_ukmap_mapdata={
   },
   data: {
     data: {
-      UK97: "1",
-      GBNIR1: "93",
-      GBNIR2: "92",
-      GBNIR3: "91",
-      GBNIR4: "90",
-      GBNIR5: "89",
-      GBNIR6: "88",
-      GBSCT1: "87",
-      GBSCT10: "86",
-      GBSCT11: "85",
-      GBSCT12: "84",
-      GBSCT13: "83",
-      GBSCT14: "82",
-      GBSCT15: "81",
-      GBSCT16: "80",
-      GBSCT17: "79",
-      GBSCT18: "78",
-      GBSCT19: "77",
-      GBSCT2: "76",
-      GBSCT20: "75",
-      GBSCT21: "74",
-      GBSCT22: "73",
-      GBSCT23: "72",
-      GBSCT24: "71",
-      GBSCT25: "70",
-      GBSCT26: "69",
-      GBSCT27: "68",
-      GBSCT28: "67",
-      GBSCT29: "66",
-      GBSCT3: "65",
-      GBSCT30: "64",
-      GBSCT31: "63",
-      GBSCT32: "62",
-      GBSCT4: "61",
-      GBSCT5: "60",
-      GBSCT6: "59",
-      GBSCT7: "58",
-      GBSCT8: "57",
-      GBSCT9: "56",
-      UK01: "54",
-      UK02: "53",
-      UK03: "52",
-      UK04: "51",
-      UK05: "50",
-      UK06: "49",
-      UK07: "48",
-      UK08: "47",
-      UK09: "46",
-      UK10: "45",
-      UK11: "44",
-      UK12: "43",
-      UK13: "42",
-      UK14: "41",
-      UK15: "40",
-      UK16: "39",
-      UK17: "38",
-      UK18: "37",
-      UK19: "36",
-      UK20: "35",
-      UK21: "34",
-      UK22: "33",
-      UK23: "32",
-      UK24: "31",
-      UK25: "30",
-      UK26: "29",
-      UK28: "28",
-      UK29: "27",
-      UK30: "26",
-      UK31: "25",
-      UK32: "24",
-      UK33: "23",
-      UK34: "22",
-      UK35: "21",
-      UK36: "20",
-      UK37: "19",
-      UK38: "18",
-      UK39: "17",
-      UK40: "16",
-      UK41: "15",
-      UK42: "14",
-      UK43: "13",
-      UK44: "12",
-      UK45: "11",
-      UK46: "10",
-      UK47: "9",
-      UK90: "8",
-      UK91: "7",
-      UK92: "6",
-      UK93: "5",
-      UK94: "4",
-      UK95: "3",
-      UK96: "2",
-      standard: "55"
+      GBNIR1: "1",
+      GBNIR2: "2",
+      GBNIR3: "3",
+      GBNIR4: "4",
+      GBNIR5: "5",
+      GBNIR6: "6",
+      GBSCT1: "7",
+      GBSCT10: "8",
+      GBSCT11: "9",
+      GBSCT12: "10",
+      GBSCT13: "11",
+      GBSCT14: "12",
+      GBSCT15: "13",
+      GBSCT16: "14",
+      GBSCT17: "15",
+      GBSCT18: "16",
+      GBSCT19: "17",
+      GBSCT2: "18",
+      GBSCT20: "19",
+      GBSCT21: "20",
+      GBSCT22: "21",
+      GBSCT23: "22",
+      GBSCT24: "23",
+      GBSCT25: "24",
+      GBSCT26: "25",
+      GBSCT27: "26",
+      GBSCT28: "27",
+      GBSCT29: "28",
+      GBSCT3: "29",
+      GBSCT30: "30",
+      GBSCT31: "31",
+      GBSCT32: "32",
+      GBSCT4: "33",
+      GBSCT5: "34",
+      GBSCT6: "35",
+      GBSCT7: "36",
+      GBSCT8: "37",
+      GBSCT9: "38",
+      UK01: "39",
+      UK02: "40",
+      UK03: "41",
+      UK04: "42",
+      UK05: "43",
+      UK06: "44",
+      UK07: "45",
+      UK08: "46",
+      UK09: "47",
+      UK10: "48",
+      UK11: "49",
+      UK12: "50",
+      UK13: "51",
+      UK14: "52",
+      UK15: "53",
+      UK16: "54",
+      UK17: "55",
+      UK18: "56",
+      UK19: "57",
+      UK20: "58",
+      UK21: "59",
+      UK22: "60",
+      UK23: "61",
+      UK24: "62",
+      UK25: "63",
+      UK26: "64",
+      UK28: "65",
+      UK29: "66",
+      UK30: "67",
+      UK31: "68",
+      UK32: "69",
+      UK33: "70",
+      UK34: "71",
+      UK35: "72",
+      UK36: "73",
+      UK37: "74",
+      UK38: "75",
+      UK39: "76",
+      UK40: "77",
+      UK41: "78",
+      UK42: "79",
+      UK43: "80",
+      UK44: "81",
+      UK45: "82",
+      UK46: "83",
+      UK47: "84",
+      UK90: "85",
+      UK91: "86",
+      UK92: "87",
+      UK93: "88",
+      UK94: "89",
+      UK95: "90",
+      UK96: "91",
+      UK97: "92",
+      standard: "93"
     }
   }
 };
